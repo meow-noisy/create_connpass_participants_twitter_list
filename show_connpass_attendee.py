e0b36a57c2e72d765c310470e3d1449eb521654f
meow <meow.noisy.code@gmail.com>
2021-07-18 15:34:58 +0900

    refactoring scraping logic

--- show_connpass_attendee.py
+++ show_connpass_attendee.py
@@ -9,6 +9,25 @@ from typing import List
 IdList = List[str]
 
 
+def get_participants_ids_in_one_page(participation_table_list) -> IdList:
+    """1ページ内のテーブルからユーザidを取得する
+
+    Args:
+        participation_table_list ([type]): BeautifulSoupのオブジェクト。ルートにテーブルを持っている
+
+    Returns:
+        IdList: ページ内のユーザidのリスト
+    """
+    participants_id_list = []
+    for participating_user in participation_table_list.select('.user'):
+        user_url = participating_user.select('.display_name a')[0]['href']
+        m = re.match('https://connpass.com/user/(.*)/',
+                     user_url)  # ユーザページのリンクからidを取得
+        participants_id_list.append(m.group(1).replace(
+            "/open", ""))  # イベント関係者の場合、リンクに/openがつくので除去
+    return participants_id_list
+
+
 def get_participants_id_list(event_url: str) -> IdList:
     """イベント参加予定者のユーザidのリストを取得
     Args:
@@ -22,20 +41,19 @@ def get_participants_id_list(event_url: str) -> IdList:
     r = requests.get(participants_url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    participants_id_list = []
+    master_participants_id_list = []
 
     # イベント関係者
-    for participation_table_list in soup.select('.concerned_area .participation_table_area'):
-        for participating_user in participation_table_list.select('.user'):
-            user_url = participating_user.select('.display_name a')[0]['href']
-            m = re.match('https://connpass.com/user/(.*)/', user_url)
-            participants_id_list.append(m.group(1))
+    for participation_table_list in soup.select('.concerned_area .participants_table'):
+        master_participants_id_list += get_participants_ids_in_one_page(
+            participation_table_list)
 
     # 一般参加者のユーザ一覧のテーブルからユーザidを取得
     for participation_table_list in soup.select('.applicant_area .participation_table_area'):
 
         # 100名以上いる場合は、別ページになっている
         another_page_list = participation_table_list.select('.empty')
+        # 別ページがある場合
         if len(another_page_list) > 0:
             another_page_url_base = another_page_list[0].select('a')[0]['href']
 
@@ -47,13 +65,12 @@ def get_participants_id_list(event_url: str) -> IdList:
                 r = requests.get(another_page_url)
                 another_soup = BeautifulSoup(r.text, 'html.parser')
 
-                participants_list = another_soup.select(
-                    '.applicant_area .participation_table_area .user')
-                for participating_user in participants_list:
-                    user_url = participating_user.select(
-                        '.display_name a')[0]['href']
-                    m = re.match('https://connpass.com/user/(.*)/', user_url)
-                    participants_id_list.append(m.group(1))
+                # ページ内のユーザidを取得
+                participation_table_list = another_soup.select(
+                    '.applicant_area .participation_table_area')
+
+                master_participants_id_list += get_participants_ids_in_one_page(
+                    participation_table_list[0])  # 別ページの場合は、1つのテーブルしかないはずなので、インデックスを指定
 
                 page_cand = another_soup.select('.paging_area ul li')
                 if "次へ" in page_cand[-1].text:
@@ -65,24 +82,20 @@ def get_participants_id_list(event_url: str) -> IdList:
                 time.sleep(1)
         # 一般参加者が100名未満の場合
         else:
-            for participating_user in participation_table_list.select('.user'):
-                user_url = participating_user.select(
-                    '.display_name a')[0]['href']
-                m = re.match('https://connpass.com/user/(.*)/', user_url)
-                participants_id_list.append(m.group(1))
+            # ページ内のユーザidを取得
+            master_participants_id_list += get_participants_ids_in_one_page(
+                participation_table_list)
 
     # 欠席者ユーザ一覧のテーブルからユーザidを取得
     for participation_table_list in soup.select('.applicant_area .cancelled_table_area'):
-        for participating_user in participation_table_list.select('.user'):
-            user_url = participating_user.select('.display_name a')[0]['href']
-            m = re.match('https://connpass.com/user/(.*)/', user_url)
-            participants_id_list.append(m.group(1))
+        master_participants_id_list += get_participants_ids_in_one_page(
+            participation_table_list)
 
     # 重複するユーザidを除外
     # イベント関係者がイベント申し込み者にいる場合があるため。
-    participants_id_list = list(set(participants_id_list))
+    master_participants_id_list = list(set(master_participants_id_list))
 
-    return participants_id_list
+    return master_participants_id_list
 
 
 url = sys.argv[1]
